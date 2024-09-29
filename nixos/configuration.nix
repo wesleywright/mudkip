a26904828dca90d4dd50433648fbe075a8ff1d14
Wesley Wright <wesley@wesleywright.me>
2024-09-29 15:44:46 -0500

    nixos: split remaining sections out into separate files

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -6,32 +6,18 @@
       ./1password.nix
       ./audio.nix    
       ./boot.nix
+      ./btrfs.nix
+      ./desktop-environment.nix
+      ./flatpak.nix
       ./hardware-configuration.nix
       ./locale.nix
+      ./networking.nix
       ./steam.nix
       ./unfree.nix
       ./users.nix
     ];
 
-  networking.hostName = "mudkip"; # Define your hostname.
-
-  services.btrfs.autoScrub = {
-    enable = true;
-    fileSystems = ["/"];
-  };
-  services.flatpak.enable = true;
-  services.displayManager.sddm.enable = true;
-  services.desktopManager.plasma6.enable = true;
-  services.xserver.enable = true;
-  services.xserver.dpi = 163;
-
-  hardware.uinput.enable = true;
-
-  # Copy the NixOS configuration file and link it from the resulting system
-  # (/run/current-system/configuration.nix). This is useful in case you
-  # accidentally delete configuration.nix.
   system.copySystemConfiguration = true;
-
   # This variable is used to indicate API compatibility, and does not need to be changed on
   # version upgrade.
   system.stateVersion = "24.05"; # Did you read the comment?
---
+++ nixos/desktop-environment.nix
@@ -0,0 +1,8 @@
+{ ... }:
+
+{
+  services.desktopManager.plasma6.enable = true;
+  services.displayManager.sddm.enable = true;
+  services.xserver.enable = true;
+  services.xserver.dpi = 162;
+}
