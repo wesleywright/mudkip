e5e9aa15b4889040004f2d228d7ac7a9165db5de
Wesley Wright <wesley@wesleywright.me>
2024-09-29 15:27:12 -0500

    nixos: split steam/1password/unfree into separate config files

---
+++ nixos/steam.nix
@@ -0,0 +1,5 @@
+{ lib, ... }:
+
+{
+  programs.steam.enable = true;
+}
---
+++ nixos/unfree.nix
@@ -0,0 +1,11 @@
+{ lib, ... }:
+
+{
+  nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
+    "1password"
+    "1password-cli"
+    "steam"
+    "steam-original"
+    "steam-run"
+  ];
+}
