5e774ada086ec150ddabfbbd5fade12c11a3495b
Wesley Wright <wesley@wesleywright.me>
2024-10-02 19:24:39 -0500

    nixos/desktop-environment: use sway as default session

--- nixos/desktop-environment.nix
+++ nixos/desktop-environment.nix
@@ -1,8 +1,13 @@
 { ... }:
 
 {
+  programs = {
+    sway.enable = true;
+  };
+
   services.desktopManager.plasma6.enable = true;
   services.displayManager.sddm.enable = true;
+  services.displayManager.defaultSession = "sway";
   services.xserver.enable = true;
   services.xserver.dpi = 162;
 }
