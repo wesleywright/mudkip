8c3ba34375526f9ba913bd93ca5b986b32c57f49
Wesley Wright <wesley@wesleywright.me>
2024-10-03 15:20:11 -0500

    home/utilities: add foot

--- home/utilities.nix
+++ home/utilities.nix
@@ -2,6 +2,9 @@
 
 {
   home.packages = [
+    # Useful when configuring Konsole/&c.
+    pkgs.foot
+
     # Progress Viewer, useful for inspecting miscellaneous byte stream operations
     pkgs.pv
     # Useful for running one-off scripts
