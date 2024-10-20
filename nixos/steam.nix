5a34e0ac3f8859a7a7e712bf60b16e783ad04e4e
Wesley Wright <wesley@wesleywright.me>
2024-10-20 18:12:15 -0500

    nixos/steam: add gamemode service for performance optimization
    Specifically installing this for Metaphor Re:Fantazio, which was having
    frequent FPS drops by default. Running with Gamemode yielded 2 to 4
    times higher framerates in cutscenes. It will likely be useful for other
    games as well.

--- nixos/steam.nix
+++ nixos/steam.nix
@@ -4,5 +4,13 @@
   # This seems to help with fully recognizing DualSense controllers.
   hardware.uinput.enable = true;
 
-  programs.steam.enable = true;
+  programs = {
+    # Gamemode allows games to enable OS performance optimizations dynamically, which can have a major boost for some games
+    gamemode.enable = true;
+
+    steam.enable = true;
+  };
+
+  # Setting CPU governor settings only works when the user is in this group
+  users.users.naptime.extraGroups = [ "gamemode" ];
 }
