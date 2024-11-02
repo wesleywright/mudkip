a0c37ca1fa13e33389b50e9cc7e1397721345e28
Wesley Wright <wesley@wesleywright.me>
2024-11-02 16:56:16 -0500

    nixos/fonts: add cjk noto variants

--- nixos/fonts.nix
+++ nixos/fonts.nix
@@ -2,7 +2,11 @@
 
 {
   fonts = {
-    packages = [ pkgs.input-fonts ];
+    packages = [
+      pkgs.input-fonts
+      pkgs.noto-fonts-cjk-sans
+      pkgs.noto-fonts-cjk-serif
+    ];
   };
 
   nixpkgs.config.input-fonts.acceptLicense = true;
