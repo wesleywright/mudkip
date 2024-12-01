dbe80c9a79605927b03eec41083b6583d2ce7133
Wesley Wright <wesley@wesleywright.me>
2024-12-01 15:24:47 -0600

    nixos: upgrade to 24.11

--- npins/default.nix
+++ npins/default.nix
@@ -80,3 +80,4 @@ else
   throw "Unsupported format version ${toString version} in sources.json. Try running `npins upgrade`"
 # Pins updated at 2024-11-02T17:06:48-05:00
 # Pins updated at 2024-11-22T18:32:57-06:00
+# Pins updated at 2024-12-01T15:16:21-06:00
