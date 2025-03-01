82e6fa49393dca20f918dd72a6a323d3bc6115ad
Wesley Wright <wesley@wesleywright.me>
2025-03-01 09:43:10 -0600

    home: add jq utility

--- home/utilities.nix
+++ home/utilities.nix
@@ -8,6 +8,9 @@
     # Useful when configuring Konsole/&c.
     pkgs.foot
 
+    # Querying language for JSON; useful for miscellaneous JSON tasks
+    pkgs.jq
+
     # Progress Viewer, useful for inspecting miscellaneous byte stream operations
     pkgs.pv
     # Useful for running one-off scripts
