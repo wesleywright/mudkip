d6304d0d942fbae72d4e7dc1037781463272e967
Wesley Wright <wesley@wesleywright.me>
2025-08-24 12:46:05 -0500

    upgrade to nixos 25.05

--- flake.nix
+++ flake.nix
@@ -2,9 +2,9 @@
   description = "mudkip system configuration files";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/release-24.11";
+    nixpkgs.url = "github:nixos/nixpkgs/release-25.05";
     home-manager = {
-      url = "github:nix-community/home-manager/release-24.11";
+      url = "github:nix-community/home-manager/release-25.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     plasma-manager = {
