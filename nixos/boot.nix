e5fdd9e14a39858aaaf44d4e9f7d6891d4352bc4
Wesley Wright <wesley@wesleywright.me>
2025-09-08 18:13:46 -0500

    update flake, switch back to default kernel (6.12)
    I continued to see issues with amdgpu on 6.6; switching back to 6.12 to
    see if that improves anything.

--- nixos/boot.nix
+++ nixos/boot.nix
@@ -1,10 +1,6 @@
 { pkgs, ... }:
 
 {
-  # The default Kernel (6.12) seemed to be causing some odd hanging issues as of
-  # 2025-08-30; pinning to 6.6 as a possible workaround.
-  boot.kernelPackages = pkgs.linuxPackages_6_6;
-
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 }
