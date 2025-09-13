0c919860686abaa88e4ce1ccc90783684f3e9a2f
Wesley Wright <wesley@wesleywright.me>
2025-09-13 11:44:19 -0500

    back to kernel 6.6
    see e5fdd9e14a39858aaaf44d4e9f7d6891d4352bc4 for previous move from 6.6
    to 6.12

--- nixos/boot.nix
+++ nixos/boot.nix
@@ -1,6 +1,10 @@
 { pkgs, ... }:
 
 {
+  # The default Kernel (6.12) seemed to be causing some odd hanging issues as of
+  # 2025-08-30; pinning to 6.6 as a possible workaround.
+  boot.kernelPackages = pkgs.linuxPackages_6_6;
+
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 }
