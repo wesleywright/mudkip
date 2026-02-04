a573d8dc8c3e7883d0a46d4776c95d4eda25c001
Wesley Wright <wesley@wesleywright.me>
2026-02-04 13:14:18 -0500

    home/music: add songrec for identifying songs

--- home/music.nix
+++ home/music.nix
@@ -2,8 +2,13 @@
 
 {
   home.packages = [
+    # Native client for Tidal
     pkgs.high-tide
+    # Open source drum machine
     pkgs.hydrogen
+    # Nice player for locally-stored files
     pkgs.lollypop
+    # Open source Shazam client
+    pkgs.songrec
   ];
 }
