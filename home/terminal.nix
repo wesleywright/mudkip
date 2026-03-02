f7c718695e17d23dcc35b538ca096b90edf10891
Wesley Wright <wesley@wesleywright.me>
2026-03-01 23:47:08 -0500

    home/terminal: disable ghostty system service
    As commented in the file, this service is intended to speedup ghostty
    window creation, but IMO it comes with more downsides than upsides.

--- home/terminal.nix
+++ home/terminal.nix
@@ -1,9 +1,13 @@
-{ pkgs, ... }:
+{ ... }:
 
 {
   programs.ghostty = {
     enable = true;
     enableFishIntegration = true;
+    # The ghostty systemd service is intended to help ghostty create new
+    # windows faster, but in practice it causes me more headaches than the
+    # speedup is worth, so I prefer to disable it.
+    systemd.enable = false;
 
     settings = {
       font-family = "Input Mono";
