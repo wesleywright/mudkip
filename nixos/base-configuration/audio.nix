8c58e8fe4c7fc619905bb94cd2f60cead77e2a31
Wesley Wright <wesley@wesleywright.me>
2026-03-25 19:01:31 -0400

    WIP: add client settings for airplay

--- nixos/base-configuration/audio.nix
+++ nixos/base-configuration/audio.nix
@@ -1,5 +1,29 @@
 { ... }:
 
 {
+  # Pipewire can make use of rtkit for performance.
+  security.rtkit.enable = true;
+
+  services.pipewire = {
+    # Pipewire is enabled by default as of NixOS 25.11, but we enable it
+    # explicitly here in case the default changes.
+    enable = true;
+
+    # RAOP stands for Remote Audio Output Protocol, which is also known as Airplay.
+    raopOpenFirewall = true;
+    extraConfig.pipewire = {
+      # Enable the RAOP Discover module, which allows using network Airplay
+      # devices as Pipewire sinks automatically.
+      "10-airplay-raop" = {
+        "context.modules" = [
+          {
+            name = "libepipewire-module-raop-discover";
+          }
+        ];
+      };
+    };
+  };
+
+  # Needed for some applications to configure audio.
   users.users.naptime.extraGroups = [ "audio" ];
 }
