5a7a573532c9456a6f4e60dd11cf9ea02f6509a1
Wesley Wright <wesley@wesleywright.me>
2026-05-06 10:46:01 -0400

    backup-bg3: switch to using BTRFS snapshots
    This will significantly reduce filesystem usage *and* make it easier to
    restore files from backups (as I'll no longer need to decompress any tar
    files).

--- home/utilities.nix
+++ home/utilities.nix
@@ -11,12 +11,14 @@ let
       set -x
 
       TIMESTAMP="$(date --utc --iso-8601=seconds)"
-      DESTINATION="$HOME/Documents/BG3/Backups/backup.$TIMESTAMP.tar.gz"
+      BACKUP_SUBVOLUME=/mnt/games/bg3-backups
+      LATEST_DIRECTORY="$BACKUP_SUBVOLUME/latest"
+      SNAPSHOTS_DIRECTORY="$BACKUP_SUBVOLUME/snapshots"
       GAME_DIRECTORY="/mnt/games/Steam/steamapps/compatdata/1086940/pfx/drive_c/users/steamuser/AppData/Local/Larian Studios/Baldur's Gate 3/PlayerProfiles/Public/"
-      SAVEGAMES="Savegames"
+      SAVEGAMES_DIRECTORY="$GAME_DIRECTORY/Savegames"
 
-      mkdir -p "$(dirname "$DESTINATION")"
-      tar czvf "$DESTINATION" -C "$GAME_DIRECTORY" "$SAVEGAMES"
+      rsync -av "$SAVEGAMES_DIRECTORY" "$LATEST_DIRECTORY"
+      sudo btrfs subvolume snapshot "$BACKUP_SUBVOLUME" "$SNAPSHOTS_DIRECTORY/$TIMESTAMP"
     '';
   };
 in
