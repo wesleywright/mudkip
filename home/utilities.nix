21be9b277db0f85003b037607e92c9af43f0083e
Wesley Wright <wesley@wesleywright.me>
2026-08-08 14:40:37 -0400

    utilities/notes: ensure directory and file exist before opening

--- home/utilities.nix
+++ home/utilities.nix
@@ -30,6 +30,8 @@ let
     text = ''
       DIRECTORY="$HOME"/notes/"$(date +%Y)"/"$(date +%m)"
       FULL_PATH="$DIRECTORY"/"$(date +%d)".md
+      mkdir -p "$DIRECTORY"
+      touch "$FULL_PATH"
       exec nvim "$FULL_PATH"
     '';
   };
