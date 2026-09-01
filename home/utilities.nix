2d772b8f947615d6071e7ecac8b9f56c08a15738
Wesley Wright <wesley@wesleywright.me>
2026-09-01 10:38:55 -0400

    utilities: add small bash script for snapshotting sha256 checksums for music

--- home/utilities.nix
+++ home/utilities.nix
@@ -35,12 +35,42 @@ let
       exec nvim "$FULL_PATH"
     '';
   };
+
+  checksum-music = pkgs.writeShellApplication {
+    name = "checksum-music";
+    runtimeInputs = [
+      pkgs.coreutils
+      pkgs.findutils
+      pkgs.python3Packages.tqdm
+    ];
+    text = ''
+      LIBRARY=/mnt/music/by-artist
+
+      pushd "$LIBRARY"
+      echo "Generating new checksum snapshot for $LIBRARY..."
+
+      find . -type f -not -name 'checksums.txt*' | tqdm | while read -r FILENAME; do
+        sha256sum "$FILENAME"
+      done >checksums.txt.new
+
+      read -r -p "New checksums ready. Press enter to view."
+      diff -u checksums.txt checksums.txt.new | less || true
+      read -r -p "Confirm new checksum snapshot? (y/N)" ANSWER
+
+      if [[ $ANSWER =~ ^[Yy]$ ]]; then
+        mv checksums.txt.new checksums.txt
+      else
+        echo "Aborting."
+      fi
+    '';
+  };
 in
 {
   home.packages = [
     # Custom scriptlets
     backup-bg3
     notes
+    checksum-music
 
     # Like cat, but prettier
     pkgs.bat
